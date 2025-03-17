24304ead41a24101722ec4e9bd2b37e552c87b09
jackp50 <jpearne9@gmail.com>
2025-03-16 18:48:37 -0600

    I updated the getComputerChoice function to give the computer a random number then assign it with either rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 console.log("Hello World");
 
 function getComputerChoice(choice) {
-
-    console.log(0);
+    choice = Math.random();
+    let randString = choice.toString();
+    if (choice <= .333) {
+        randString = "Rock";
+    }
+    else if (choice <= .666) {
+        randString = "Paper"
+    }
+    else {
+        randString = "Scissors"
+    }
+    
+    console.log(randString);
 }
 
 
 function getHumanChoice(pick) {
 
-pick = prompt("Please choose Rock, Paper, or Scissors")
+    pick = prompt("Please choose Rock, Paper, or Scissors")
 
     console.log(pick);
 
 }
 let humanScore = 0;
 let computerScore = 0;
+
 getComputerChoice();
 getHumanChoice();
