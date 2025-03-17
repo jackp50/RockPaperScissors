f30ba4b67598bd578e40a6d666b03c0b97290b47
jackp50 <jpearne9@gmail.com>
2025-03-16 18:37:24 -0600

    Added getHumanChoice and getComputerChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+function getComputerChoice(choice) {
+
+    console.log(0);
+}
+
+
+function getHumanChoice(pick) {
+
+pick = prompt("Please choose Rock, Paper, or Scissors")
+
+    console.log(pick);
+
+}
+let humanScore = 0;
+let computerScore = 0;
+getComputerChoice();
+getHumanChoice();
